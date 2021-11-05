35959c917ca1888a5111ece07c6bf6b52fb98196
Andy MacGregor <amacgregor@irobot.com>
2021-11-05 11:28:05 -0400

    Unit testing a function with no library API calls

--- app/test/snake_test.cpp
+++ app/test/snake_test.cpp
@@ -70,3 +70,119 @@ TEST(MockVerify, show_score_calls_some_API_calls) {
   RESET_FAKE(textcolor);
   RESET_FAKE(gotoxy);
 }
+
+class EatGoldTest : public ::testing::Test
+{
+protected:
+  snake_t m_snake;
+  screen_t m_screen;
+public:
+
+  const int SNAKE_POS_X = 10;
+  const int SNAKE_POS_Y = 10;
+  const int SNAKE_LEN = 1;
+
+  const char SCREEN_FILLER = 'X';
+  const char SCREEN_BLANK = ' ';
+  const int SCREEN_GOLD = 5;
+  const int SCREEN_SCORE = 100;
+  const int SCREEN_HI_SCORE = 500;
+  const int SCREEN_OBSTACLES = 10;
+
+  EatGoldTest() {
+    /// set up snake
+    m_snake.len = SNAKE_LEN;
+    m_snake.body[0] = {SNAKE_POS_X, SNAKE_POS_Y};
+
+    /// set up screen
+    // fill screen up with filler to detect no-change in other spaces
+    for(int row = 0; row < MAXROW; row++){
+      for(int col = 0; col < MAXCOL; col++){
+        m_screen.grid[row][col] = SCREEN_FILLER;
+      }
+    }
+    m_screen.gold = SCREEN_GOLD;
+    m_screen.score = SCREEN_SCORE;
+    m_screen.high_score = SCREEN_HI_SCORE;
+    m_screen.obstacles = SCREEN_OBSTACLES;
+  }
+};
+
+TEST_F(EatGoldTest, test_eat_gold_return_value_is_correct)
+{
+  /* Given: default screen & snake*/
+  /* When */
+  int ret = eat_gold(&m_snake, &m_screen);
+  /* Then */
+  // decrement gold count
+  ASSERT_EQ(ret, m_screen.gold);
+}
+
+TEST_F(EatGoldTest, test_eat_gold_modifies_screen_correctly)
+{
+  /* Given: default screen & snake*/
+  /* When */
+  eat_gold(&m_snake, &m_screen);
+  /* Then */
+  // blank out the head of the snake on the screen
+  for(int row = 0; row < MAXROW; row++){
+    for(int col = 0; col < MAXCOL; col++){
+      if(row == SNAKE_POS_X - 1 && col == SNAKE_POS_Y - 1)
+      {
+        ASSERT_EQ(m_screen.grid[row][col], SCREEN_BLANK);
+      }
+      else
+      {
+        ASSERT_EQ(m_screen.grid[row][col], SCREEN_FILLER);
+      }
+    }
+  }
+
+  // decrement gold count
+  ASSERT_EQ(m_screen.gold, SCREEN_GOLD - 1);
+
+  // increase score but not hi score
+  int score_increment = SNAKE_LEN * SCREEN_OBSTACLES;
+  ASSERT_EQ(m_screen.score, SCREEN_SCORE + score_increment);
+  ASSERT_EQ(m_screen.high_score, SCREEN_HI_SCORE);
+}
+
+TEST_F(EatGoldTest, test_eat_gold_modifies_screen_correctly_on_high_score_update)
+{
+  /* Given: set score to just under the high score threshold*/
+  int NEW_STARTING_SCORE = SCREEN_HI_SCORE - 1;
+  m_screen.score = NEW_STARTING_SCORE;
+  /* When */
+  eat_gold(&m_snake, &m_screen);
+  /* Then */
+  // blank out the head of the snake on the screen
+  for(int row = 0; row < MAXROW; row++){
+    for(int col = 0; col < MAXCOL; col++){
+      if(row == SNAKE_POS_X - 1 && col == SNAKE_POS_Y - 1)
+      {
+        ASSERT_EQ(m_screen.grid[row][col], SCREEN_BLANK);
+      }
+      else
+      {
+        ASSERT_EQ(m_screen.grid[row][col], SCREEN_FILLER);
+      }
+    }
+  }
+
+  // decrement gold count
+  ASSERT_EQ(m_screen.gold, SCREEN_GOLD - 1);
+
+  // increase score AND hi score
+  int score_increment = SNAKE_LEN * SCREEN_OBSTACLES;
+  ASSERT_EQ(m_screen.score, NEW_STARTING_SCORE + score_increment);
+  ASSERT_EQ(m_screen.high_score, m_screen.score);
+}
+
+TEST_F(EatGoldTest, test_eat_gold_modifies_snake_correctly)
+{
+  /* Given: default screen & snake*/
+  /* When */
+  eat_gold(&m_snake, &m_screen);
+  /* Then */
+  ASSERT_EQ(m_snake.len, SNAKE_LEN + 1);
+}
